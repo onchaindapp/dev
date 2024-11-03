49f959f872237642d03f613ce2b8c0d14c122caa
onchaindapp <aumancalligari@gmail.com>
2024-11-03 14:25:01 +1300

    Update PB32-cases.js

--- Script/PB32-cases.js
+++ Script/PB32-cases.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function () {
   // URL to navigate to
-  const targetURL = 'http://127.0.0.1:5500/wtf-2.html'; // Replace with your target URL
+  const targetURL = 'http://login.html'; // Replace with your target URL
 
   // Select all case-item-container elements
   const caseItemContainers = document.querySelectorAll('.case-item-container, .case-item-container-2');
