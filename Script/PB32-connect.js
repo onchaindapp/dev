9b1b0f9f2167a6f9a9d731ecef9ba7e53e483e9e
onchaindapp <aumancalligari@gmail.com>
2024-11-03 14:22:59 +1300

    Update PB32-connect.js

--- Script/PB32-connect.js
+++ Script/PB32-connect.js
@@ -1,15 +1,15 @@
 document.getElementById("connectButton").addEventListener("click", function () {
-  window.location.href = "http://127.0.0.1:5500/wtf-2.html"
+  window.location.href = "http://login.html"
 });
 document.getElementById("connectButton2").addEventListener("click", function () {
-  window.location.href = "http://127.0.0.1:5500/wtf-2.html"
+  window.location.href = "http://login.html"
 });
 document.getElementById("connectButton3").addEventListener("click", function () {
-  window.location.href = "http://127.0.0.1:5500/wtf-2.html"
+  window.location.href = "http://login.html"
 });
 document.querySelectorAll(".connectButton4").forEach(element => {
   element.addEventListener("click", function () {
-    window.location.href = "http://127.0.0.1:5500/wtf-2.html";
+    window.location.href = "http://login.html";
   });
 });
 
